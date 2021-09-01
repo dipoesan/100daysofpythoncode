2173d4148f8e3b6f0ac5ed19799c74a26009fc76
dipoesan <esan.dipo@gmail.com>
2021-09-01 09:11:46 +0800

    adding my code from vscode to github

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#Welcome message comes here
+print ("Welcome to my band name generation app.")
+
+#I have assigned variables to store values gotten from the input functions
+city = (input("What city did you grow up in?\n"))
+
+pet = (input("What is the name of your pet?\n"))
+
+#I concatenated the variables and string
+print ("The name of your band could be " + city + " " + pet)
