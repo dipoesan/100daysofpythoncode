354daaae541377b0e6f1fb72862c961c8fe630fe
dipoesan <esan.dipo@gmail.com>
2021-09-02 19:22:23 +0800

    Day3, Exercise 1

---
+++ PracticeFile.py
@@ -0,0 +1,63 @@
+# num_char = len (input("What is your name?\n"))
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters")
+
+# 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# print (type(two_digit_number))
+
+# print (int(two_digit_number [0]) + int(two_digit_number [1]))
+
+# 🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+
+# print (type(height))
+
+# print (type(weight))
+
+# height_squared = (float(height)) ** 2
+
+# BMI = float(weight) / height_squared
+
+# print (int (BMI))
+
+# 🚨 Don't change the code below 👇
+# age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# ninety_days = 90
+
+# days = (ninety_days - int(age)) * 365 
+
+# weeks = (ninety_days - int(age)) * 52
+
+# months = (ninety_days - int(age)) * 12
+
+# print (f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your height in cm? "))
+
+if height>=120:
+    print("You can ride on the rollercoaster")
+    age = int(input("What is your age? "))
+    if age<=18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("You cannot ride on the rollercoaster")
