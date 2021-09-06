16ff286d6e27bba5ec237b4c6c545915e7c26224
dipoesan <esan.dipo@gmail.com>
2021-09-06 19:31:54 +0800

    Updated my notes in the practice file

---
+++ Day 6/PracticeFile.py
@@ -0,0 +1,14 @@
+# All about functions
+# They end with a parentheses "()"
+# Examples are "print()", "len()"
+# We can also define/create a function of our own
+# This is done by using "def" keyword then  name of our function
+# Then we add all instructions/lines of code we wany to execute
+# We can then call the function later b y specifying the name of the function and adding the parentheses
+ 
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
