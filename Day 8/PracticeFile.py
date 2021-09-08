3868221cbe25c5a628a05fbaaaafae7ae7ae792f
dipoesan <esan.dipo@gmail.com>
2021-09-08 19:19:07 +0800

    Day 8, exercise one completed

---
+++ Day 8/PracticeFile.py
@@ -0,0 +1,19 @@
+# def greet ():
+#     print("Hello")
+#     print("New")
+#     print("World")
+# greet()   
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
+
+# greet_with_name("Angela")
+
+# Functions with more than one input
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Dipo Esan", "Canada")
